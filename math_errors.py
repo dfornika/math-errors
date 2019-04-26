759db0b80795324a1a50c53dbbb2084b97611bc4
Dan Fornika <dfornika@gmail.com>
2019-04-25 22:16:24 -0700

    break test_00
    ...or is it 4?

--- math_errors.py
+++ math_errors.py
@@ -5,7 +5,7 @@ import unittest
 class TestMathErrors(unittest.TestCase):
     def test_00(self):
         x = 1 + 1
-        answer = 2
+        answer = 4
         self.assertEqual(x, answer)
 
 if __name__ == '__main__':
